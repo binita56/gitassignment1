7963140f8845cd076a253c8ab481457d2ef4361d
binita shrestha <stha.binita56@gmail.com>
2022-03-04 20:36:12 +0545

    first commit

---
+++ myassignment.js
@@ -0,0 +1,17 @@
+let x=2;
+let y=5;
+let z=23;
+if(x>y&&x>z)
+{
+    console.log("x is larger")
+}
+else if(y>x&&y>z)
+{
+    console.log("y is larger")
+}
+else
+{
+    console.log("z is larger")
+}
+    
+
